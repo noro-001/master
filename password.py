048583ac36ab9c3d9e7708a107dc3006c5653d90
noro-001 <nhovhannisyyan.01@gmail.com>
2024-09-23 22:52:04 +0400

    Add files via upload

---
+++ password.py
@@ -0,0 +1,8 @@
+print("Welcome to our program! ")
+
+user_city = input("Enter the city where you grow up: ")
+user_pet = input("Enter your pet name: ")
+user_password = user_city + user_pet
+
+print("_________________________________")
+print("So Your password is: ", user_password) 
