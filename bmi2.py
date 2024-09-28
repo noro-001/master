2a9f898161477b612afdfb1da3f3ea9534c3523a
noro-001 <nhovhannisyyan.01@gmail.com>
2024-09-28 21:06:05 +0400

    Add files via upload

---
+++ bmi2.py
@@ -0,0 +1,15 @@
+user_weight = eval(input("Enter your weight: "))
+user_height = eval(input("Enter your height: "))
+
+bmi = user_weight / user_height ** 2 
+
+if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi > 18.5 and bmi < 25:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi > 25 and bmi < 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi >= 30 and bmi < 35:
+    print(f"Your BMI is {bmi}, you are obese.")
+elif bmi >= 35:
+    print(f"Your BMI is {bmi}, you are clinicaly obese.")
